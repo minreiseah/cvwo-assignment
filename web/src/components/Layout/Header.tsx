3674a8a8be92e32386a6790f03d26134c6e79fe0
minirei <minrei101@gmail.com>
2022-12-10 22:11:23 +0800

    integrated Chakra UI

--- web/src/components/Layout/Header.tsx
+++ web/src/components/Layout/Header.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const Header = () => {
-  return null;
+import {
+  Box,
+  Flex,
+  HStack,
+} from '@chakra-ui/react'
+
+
+const Header: React.FC = () => {
+  return (
+    <Box w={"100%"} bg={"black"}>
+      <HStack>
+        <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
+      </HStack>
+
+    </Box>
+
+  )
 }
 
 export default Header;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -8,17 +8,36 @@ import { store } from './app/store';
 // react-router
 import { BrowserRouter } from 'react-router-dom';
 
+// Chakra UI
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
+
 import App from './App';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+// Extending Chakra UI theme to include custom colours, fonts
+const colours = {
+  primary: {
+    1: '#5EB2A5', // teal
+    2: '#F8C6D2' // pink
+  },
+  secondary: {
+    1: '#DFF2F8', // cyan
+    2: '$4B8E9A', // malachite
+  }
+}
+
+const theme = extendTheme({ colours })
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
