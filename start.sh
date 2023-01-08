4e825cf2a13b71948557362d08a46b090e5f4518
Min Rei <58839570+minreiseah@users.noreply.github.com>
2023-01-08 18:55:32 +0800

    Separated development and production (#6)
    * updated environment variables
    - docker-compose configured propertly for solely local development
    
    * db migration now done in Go, not Bash

--- start.sh
+++ start.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-echo "run db migration"
+# echo "run db migration"
 # /app/migrate -path /app/migration -database "postgresql://root:toor@postgres15:5432/cvwo_forum?sslmode=disable" -verbose up
-/app/migrate -path /app/migration -database "postgres://root:EikF32o3XditPPgEATyjygwVQPyNFRWm@dpg-cesnbgirrk0dan0ldiag-a.singapore-postgres.render.com/db_0b4c" -verbose up
 
 echo "start the backend"
 exec "$@"
